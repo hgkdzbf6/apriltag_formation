be34fbfbdddb6ff07dda72ce31218429bb29666c
zbf <hgkdzbf6@qq.com>
2018-01-01 08:30:25 +0800

    create auto_download.sh

---
+++ auto_download.sh
@@ -0,0 +1,12 @@
+cd ./aprilslam && git pull origin +master 
+cd ..
+cd ./hector_quadrotor && git pull origin +master 
+cd ..
+cd ./hector_localization && git pull origin catkin
+cd ..
+cd ./hector_gazebo &&  git pull origin +master
+cd ..
+cd ./hector_models &&  git pull origin  kinetic-devel
+cd  ..
+git pull origin +master
+
